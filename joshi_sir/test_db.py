b9137b321bf0d1a218be7aa8a5ce2eda5f52eece
snehasish <rsnhasish125@gmail.com>
2024-03-01 12:30:56 +0530

    final

--- joshi_sir/test_db.py
+++ joshi_sir/test_db.py
@@ -5,18 +5,18 @@ Testing the database connection
 import sqlite3
 
 # Connect to the SQLite database
-conn = sqlite3.connect('my_database.db')
+conn = sqlite3.connect("C:/Users/Snehasish/Downloads/a_new/my_database.db")
 
 # Create a cursor object
 cur = conn.cursor()
 
 def query_db(query:str) -> list:
     #  Execute the SQL query
-    cur.execute("SELECT * FROM data WHERE tag LIKE '%{}%'".format(query))
+    cur.execute("SELECT * FROM data WHERE date LIKE '%{}%'".format(query))
     return cur.fetchall()
 
 # Fetch all the rows
-# print(_:=query_db('jay'))
+print(_:=query_db('2024-02-29'))
 # rows = _
 # print((rows[-1][-2]))
 
@@ -25,4 +25,4 @@ def query_db(query:str) -> list:
 #     print(row)
 
 # Close the connection
-# conn.close()
+conn.close()
